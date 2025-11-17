c176ec65a40287f84444a0e2a341a034a87f5035
juha <0toutestpossible@gmail.com>
2025-11-17 23:57:10 +0900

    전반적 오류 수정(ing)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.recyclerview:1.3.2")
-    implementation("androidx.cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
 
     // ===== Room (SQLite ORM) 추가 =====
     val roomVersion = "2.6.1"
